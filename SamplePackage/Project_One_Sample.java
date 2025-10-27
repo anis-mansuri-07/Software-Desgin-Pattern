588469717d910278da021b71236339eabc80ccb2
MAHAMMEDANIS S MANSURI <144646755+anis-mansuri-07@users.noreply.github.com>
2025-10-28 02:19:55 +0530

    Delete SamplePackage directory

--- SamplePackage/Project_One_Sample.java
+++
@@ -1,9 +0,0 @@
-package SamplePackage;
-
-public class Project_One_Sample {
-    public static void main(String[] args) {
-        System.out.println(Project_Two_Sample.staticVarOne);
-
-        System.out.println(Project_Two_Sample.staticVarOne);
-    }
-}
